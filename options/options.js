f0e4e950843a2c2581e18391b99daff99809adf9
Crowdbotics <team@crowdbotics.com>
2023-06-11 20:34:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dabu-41421",
+  url: "https://dabu-41421.botics.co",
+  api: "https://dabu-41421.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
